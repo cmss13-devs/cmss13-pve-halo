b316b07590dfb3dc569476061838a7fec55ba974
The32bitguy <35638613+The32bitguy@users.noreply.github.com>
2026-02-16 02:40:07 +0000

    initial

--- tgui/packages/tgui/interfaces/HumanAISpawner.tsx
+++ tgui/packages/tgui/interfaces/HumanAISpawner.tsx
@@ -1,14 +1,7 @@
 import { useState } from 'react';
 
 import { useBackend } from '../backend';
-import {
-  Box,
-  Button,
-  Collapsible,
-  Divider,
-  Section,
-  Stack,
-} from '../components';
+import { Box, Button, Divider, Dropdown, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
 type AIEquipmentPreset = {
@@ -19,61 +12,237 @@ type AIEquipmentPreset = {
 
 type BackendContext = {
   presets: { [key: string]: AIEquipmentPreset[] };
+  selected_faction: string;
+  selectable_factions: string[];
+  spawn_ai: boolean;
+  paradrop: boolean;
+  outfit: number;
+  desc: number;
+  spawn_click_intercept: boolean;
+  equipment_settings: string[];
+  selected_equipment: string;
+  species_settings: string[];
+  species_selected: string;
 };
 
 export const HumanAISpawner = (props) => {
   const { data, act } = useBackend<BackendContext>();
   const [chosenPreset, setPreset] = useState<AIEquipmentPreset | null>(null);
+  const [viewingFaction, setViewingFaction] = useState<string | null>(null);
   const { presets } = data;
+
+  const factionOptions = Object.keys(presets);
+
   return (
     <Window title="Human AI Spawner" width={800} height={900}>
       <Window.Content>
         <Stack fill vertical>
           <Stack fill>
+            {/* left panel*/}
             <Stack.Item grow mr={1}>
-              <Section fill height="100%">
-                {Object.keys(presets).map((dictKey) => (
-                  <Collapsible title={dictKey} key={dictKey} color="good">
-                    {presets[dictKey].map((squad) => (
-                      <Box pb={'12px'} key={squad.path}>
+              <Box mb={1}>
+                <Dropdown
+                  placeholder="Filter by Faction..."
+                  options={factionOptions}
+                  selected={viewingFaction}
+                  width="100%"
+                  onSelected={(value) => setViewingFaction(value)}
+                />
+              </Box>
+              <Section
+                fill
+                scrollable
+                title="Presets"
+                buttons={
+                  <Button
+                    textAlign="center"
+                    width="100%"
+                    onClick={() => act('add_preset')}
+                  >
+                    Add Preset
+                  </Button>
+                }
+              >
+                <Divider />
+                {viewingFaction ? (
+                  <Box mt={1}>
+                    {presets[viewingFaction]?.map((squad) => (
+                      <Box pb="12px" key={squad.path}>
                         <Button
                           fontSize="15px"
                           textAlign="center"
-                          selected={squad === chosenPreset}
+                          selected={squad.path === chosenPreset?.path}
+                          // key={squad?.path}
                           width="100%"
-                          key={squad.path}
-                          onClick={() => setPreset(squad)}
+                          onClick={() => {
+                            setPreset(squad);
+                            act('remember_path', {
+                              path: squad.path,
+                              selected_faction: data.selected_faction,
+                              selected_equipment: data.selected_equipment,
+                              species_selected: data.species_selected,
+                            });
+                          }}
                         >
                           {squad.name}
                         </Button>
                       </Box>
                     ))}
-                  </Collapsible>
-                ))}
+                  </Box>
+                ) : (
+                  <Box textAlign="center" italic color="label">
+                    Select a faction to view presets
+                  </Box>
+                )}
               </Section>
             </Stack.Item>
             <Divider vertical />
             <Stack.Item width="30%">
-              <Section title="Selected Preset">
+              <Section
+                title="Selected Preset"
+                buttons={
+                  <Button.Checkbox
+                    checked={data.desc}
+                    icon="eye-slash"
+                    onClick={() => act('hide_desc')}
+                  />
+                }
+              >
                 {chosenPreset !== null ? (
                   <Stack vertical>
-                    <Stack.Item>{chosenPreset.description}</Stack.Item>
+                    <Stack.Item>
+                      {data.desc === 1 && chosenPreset.description}
+                    </Stack.Item>
                     <Stack.Item>
                       <Button
                         textAlign="center"
                         width="100%"
+                        selected={data.spawn_click_intercept}
                         onClick={() =>
                           act('create_ai', {
                             path: chosenPreset.path,
+                            selected_faction: data.selected_faction,
+                            selected_equipment: data.selected_equipment,
                           })
                         }
                       >
-                        Spawn
+                        {data.outfit === 1 ? 'Outfit' : 'Click Spawn'}
                       </Button>
+                      {/* path: selected_faction: etc get passed as the params["path"]*/}
                     </Stack.Item>
+                    <Stack.Item>
+                      <Dropdown
+                        width="100%"
+                        options={data.selectable_factions}
+                        selected={data.selected_faction}
+                        displayText={
+                          <span
+                            style={{
+                              color: data.selected_faction
+                                ?.toLowerCase()
+                                .includes('hive')
+                                ? 'purple'
+                                : data.selected_faction !== chosenPreset.faction
+                                  ? 'pink'
+                                  : 'white',
+                            }}
+                          >
+                            {data.selected_faction}
+                          </span>
+                        }
+                        onSelected={(selected_faction) => {
+                          act('set_selected_faction', {
+                            selected_faction: selected_faction,
+                            path: chosenPreset.path,
+                          });
+                        }}
+                      />
+                    </Stack.Item>
+
+                    <Button.Checkbox
+                      checked={data.paradrop}
+                      onClick={() => {
+                        act('paradrop_toggle');
+                      }}
+                    >
+                      Parachute
+                    </Button.Checkbox>
+                    <Button.Checkbox
+                      checked={data.outfit}
+                      onClick={() => {
+                        act('outfit');
+                      }}
+                    >
+                      Click gives outfit
+                    </Button.Checkbox>
+                    <Section title="Additional Options" />
+                    <Dropdown
+                      width="100%"
+                      options={data.species_settings}
+                      selected={data.species_selected}
+                      displayText={
+                        <span
+                          style={{
+                            color:
+                              data.species_selected
+                                ?.toLowerCase()
+                                .includes('Sangheli') ||
+                              data.selected_faction
+                                ?.toLowerCase()
+                                .includes('synth')
+                                ? 'blue'
+                                : data.species_selected !== 'Human'
+                                  ? 'orange'
+                                  : 'white',
+                          }}
+                        >
+                          {data.species_selected}
+                        </span>
+                      }
+                      onSelected={(species_selected) => {
+                        act('set_selected_species', {
+                          species_selected: species_selected,
+                        });
+                      }}
+                    />
+                    <Dropdown
+                      width="100%"
+                      options={data.equipment_settings}
+                      selected={data.selected_equipment}
+                      displayText={
+                        <span
+                          style={{
+                            color:
+                              data.selected_equipment === 'Birthday Suit'
+                                ? 'pink'
+                                : data.selected_equipment !== 'Full Equipment'
+                                  ? 'orange'
+                                  : 'white',
+                          }}
+                        >
+                          {data.selected_equipment}
+                        </span>
+                      }
+                      onSelected={(selected_equipment) => {
+                        act('set_selected_equipment', {
+                          selected_equipment: selected_equipment,
+                          path: chosenPreset.path,
+                        });
+                      }}
+                    />
+                    <Button.Checkbox
+                      checked={data.spawn_ai}
+                      onClick={() => {
+                        act('human_spawn_ai_toggle');
+                      }}
+                    >
+                      AI
+                    </Button.Checkbox>
                   </Stack>
                 ) : (
-                  <div />
+                  <Box textAlign="center" italic color="label">
+                    No preset selected
+                  </Box>
                 )}
               </Section>
             </Stack.Item>
